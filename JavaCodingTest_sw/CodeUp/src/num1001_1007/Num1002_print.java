c85912be03be83080dfe751787b321f8b730e42c
swgic <jinsowon82@gmail.com>
2024-08-15 03:13:28 +0900

    Delete JavaCodingTest_sw/CodeUp directory
    delete

--- JavaCodingTest_sw/CodeUp/src/num1001_1007/Num1002_print.java
+++
@@ -1,9 +0,0 @@
-package num1001_1007;
-
-public class Num1002_print {
-
-public static void main(String[] args) {
-		System.out.printf("%s %s", "Hello", "World");	
-	}
-
-}
--- JavaCodingTest_sw/CodeUp/src/num1001_1007/Num1004_print.java
+++
@@ -1,9 +0,0 @@
-package num1001_1007;
-
-public class Num1004_print {
-	public static void main(String[] args) {
-		
-		System.out.printf("\'%s\'", "Hello");	
-	}
-
-}
